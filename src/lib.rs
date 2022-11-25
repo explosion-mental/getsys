784d379dd4d5032a2d4e3c7c78c7399a59e39a42
explosion-mental <explosion0mental@gmail.com>
2022-11-25 10:58:06 -0500

    root commit
    basic skeleton

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+struct Cpu {}
+
+impl Cpu {
+    fn turbo() -> bool {
+        true
+    }
+
+    fn cores() -> u32 {
+        8
+    }
+
+    fn perc() -> u32 {
+        100
+    }
+
+    fn temp() -> u32 {
+        42
+    }
+}
+
