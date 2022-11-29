3561d94e29b2cc67e20a09fdc17f10db71263fa9
explosion-mental <explosion0mental@gmail.com>
2022-11-28 21:52:02 -0500

    add PerCore example

--- examples/cpu.rs
+++ examples/cpu.rs
@@ -1,6 +1,6 @@
 extern crate getsys;
 
-use getsys::Cpu;
+use getsys::{Cpu, PerCpu};
 
 fn main() {
     let x = if Cpu::turbo() == true { "enabled" } else { "disabled" };
@@ -11,4 +11,20 @@ fn main() {
     println!("Number of cores is: {}", y);
     println!("Average temperature: {} °C", z);
     println!("Average cpu percentage: {:.2}%", m);
+
+    for (i, e) in PerCpu::freq().iter().enumerate() {
+        println!("Core {} has frequency of: {}", i, e);
+    }
+
+    println!("");
+
+    for (i, e) in PerCpu::governor().iter().enumerate() {
+        println!("Core {} uses {}", i, e);
+    }
+
+    println!("");
+
+    for (i, e) in PerCpu::driver().iter().enumerate() {
+        println!("Core {} uses driver {}", i, e);
+    }
 }
