00e6983ac136c9b397460b1caf677c655c47d8a4
explosion-mental <explosion0mental@gmail.com>
2023-03-05 13:50:53 -0500

    [lib] use `//!`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! `getsys` Library to get _some_ system _stuff_
-/// The plan is to add some stable support (minor versions) and then release v2 (major version) that
-/// implements proper error handling using thiserror macros.
+//! The plan is to add some stable support (minor versions) and then release v2 (major version) that
+//! implements proper error handling using thiserror macros.
 
 /// Cpu functions
 pub mod cpu;
