db8c21ecb23f73baa58a4fd0638716af02730c8f
explosion-mental <explosion0mental@gmail.com>
2023-03-06 10:31:05 -0500

    new patch version: 1.1.1 - [percpu] encapsulate logic into a fn + enum

--- src/percpu.rs
+++ src/percpu.rs
@@ -5,6 +5,36 @@
 use std::fs;
 use glob::glob;
 
+/// possible percpu values
+enum Kind {
+    Governor,
+    Freq,
+    Driver,
+}
+
+/// Wrapper to get percpu values (avoids duplicating code). This is called by all other functions
+/// on percpu. Adding more values should be easy enough
+//TODO error handling? glob shouldn't return an error, if it does the path doesn't exist and values cannot be obtained..
+fn get_value(n: Kind) -> Vec<String> {
+    let mut data = vec![];
+
+    let path = match n {
+        Kind::Governor => "/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor",
+        Kind::Freq     => "/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_cur_freq",
+        Kind::Driver   => "/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_driver",
+    };
+
+    for entry in glob(path).expect("glob pattern is ok") {
+        let info = match entry {
+            Ok(path) => fs::read_to_string(path).expect("/sys fs should be avaliable for reading.").trim().to_owned(),
+            Err(e) => { eprintln!("{e}"); "Unavaliable".to_owned() },
+        };
+        data.push(info);
+    }
+
+    data
+}
+
 /// Returns a vector of strings that represents the scaling governor the
 /// respective cpu is using, from cpu 0 to cpu X.
 /// You can view avaliable governors with:
@@ -12,18 +42,7 @@ use glob::glob;
 ///
 /// The vector is as large as the number of cpus.
 pub fn governor() -> Vec<String> {
-    let mut govs: Vec<String> = Vec::new();
-
-    for entry in glob("/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor").expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => govs.push(
-                fs::read_to_string(path).expect("sysfs file shoudln't return an error").trim().to_string()
-            ),
-            Err(e) => println!("{:?}", e),
-        }
-    }
-
-    govs
+    get_value(Kind::Governor)
 }
 
 /// Returns a vector of strings that represents the current frequency in kHz
@@ -31,34 +50,12 @@ pub fn governor() -> Vec<String> {
 /// The vector is as large as the number of cpus.
 // TODO use a more precise type than a String?
 pub fn freq() -> Vec<String> {
-    let mut govs: Vec<String> = Vec::new();
-
-    for entry in glob("/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_cur_freq").expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => govs.push(
-                fs::read_to_string(path).expect("sysfs file shoudln't return an error").trim().to_string()
-            ),
-            Err(e) => println!("{:?}", e),
-        }
-    }
-
-    govs
+    get_value(Kind::Freq)
 }
 
 /// Returns a vector of strings that represents the driver, or _policy_, the
 /// respective cpu is using, from cpu 0 to cpu X.
 /// The vector is as large as the number of cpus.
 pub fn driver() -> Vec<String> {
-    let mut govs: Vec<String> = Vec::new();
-
-    for entry in glob("/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_driver").expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => govs.push(
-                fs::read_to_string(path).expect("sysfs file shoudln't return an error").trim().to_string()
-            ),
-            Err(e) => println!("{:?}", e),
-        }
-    }
-
-    govs
+    get_value(Kind::Driver)
 }
