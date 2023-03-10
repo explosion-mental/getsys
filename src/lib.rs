47e9dea510b156331d9d57a419c4360a1914ba76
explosion-mental <explosion0mental@gmail.com>
2023-03-10 17:38:35 -0500

    new patch version: 1.1.3 - update "Thoughts"

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #![doc = include_str!("../README.md")]
 //! \n
-//! # Thoughts
-//! * [X] add some stable support (minor versions)
-//! * [ ] release v2 (major version) that implements proper error handling using thiserror macros
-//! (This is still an idea, given that this lib **only** reads /sys fs, which should exist in order
-//! for this to work)
+//! # Thoughts (these are not TODO's)
+//! * release v2 (major version) that implements proper error handling using thiserror macros
+//!   (This is still an idea, given that this lib **only** reads /sys fs, which should exist in order
+//!   for this to work)
+//! * use `num_cpus` crate, instead of [`glob`]ing the results. This removes searching for the
+//!   paths, and just directly write to it. Can also remove errors from a glob pattern, although it
+//!   should not be a problem given how simple it is (`../cpu[0-9]*/..`)
 
 /// Cpu functions
 pub mod cpu;
